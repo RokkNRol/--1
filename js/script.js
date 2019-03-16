9977d135580160d4ad11b24beb95e896b825dbab
RokkNRol <himera80@mail.ru>
2019-03-16 18:32:15 +0200

    practics

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
-  // Your code below this line
-  var result = 'myNoun' + 'myAdjective' + 'myVerb' + 'myAdverb';
-
-  // Your code above this line
-  return result;
-}
-
-// Change the words here to test your function
-wordBlanks("dog", "big", "ran", "quickly");
-wordBlanks("cat", "little", "hit", "slowly");
+$(function() {
+   $('.top-slider').slick({
+      arrows:false,
+      dots:true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      fade: true
+   });
+});
