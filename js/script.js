ca28b7369a87ae0a981073c0de18cf7ba2698e9e
RockNRoll <himera80@mail.ru>
2019-03-17 18:04:44 +0200

    @media

--- js/script.js
+++ js/script.js
@@ -6,4 +6,21 @@ $(function() {
       autoplaySpeed: 3000,
       fade: true
    });
+   	$(".top__to-go").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 900);
+	});
+$('.menu--btn').click(function() {
+   $('.header--menu-list').slideToggle();
 });
+});
+
